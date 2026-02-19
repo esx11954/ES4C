85f3454c30b05f506150596ac64da9735365921d
esx11954 <42484592+esx11954@users.noreply.github.com>
2026-02-19 15:37:33 +0900

    Update homepage title and trigger deployment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,8 +33,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`CCNA 対策`}
+      description="Cisco Certified Network Associate 過去問演習サイト">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
